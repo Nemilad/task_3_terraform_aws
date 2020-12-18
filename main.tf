56475cbf7d5362051cf34c5350e2d7044c5fd7ca
Nemilad <Nik155@list.ru>
2020-12-18 21:28:45 +0300

    DB and API

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ resource "aws_instance" "instance" {
   user_data = <<-EOF
         #!/bin/bash
         echo "Hello world" > index.html
-        nohup busybox httpd -f -p 8080 &
+        nohup busybox httpd -f -p ${var.server_port} &
         EOF
 
   vpc_security_group_ids = [aws_security_group.asg_instance.id]
-        
+
   tags = {
     Name = "terraform_webpage"
   }
@@ -24,10 +24,85 @@ resource "aws_instance" "instance" {
 resource "aws_security_group" "asg_instance" {
   name = "terraform-asg"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+resource "aws_dynamodb_table_item" "example" {
+  table_name = aws_dynamodb_table.example.name
+  hash_key   = aws_dynamodb_table.example.hash_key
+
+  item = <<ITEM
+{
+  "exampleHashKey": {"S": "something"},
+  "one": {"N": "11111"},
+  "two": {"N": "22222"},
+  "three": {"N": "33333"},
+  "four": {"N": "44444"}
+}
+ITEM
+}
+
+resource "aws_dynamodb_table" "example" {
+  name           = "example-name"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "exampleHashKey"
+
+  attribute {
+    name = "exampleHashKey"
+    type = "S"
+  }
+}
+
+resource "aws_api_gateway_rest_api" "MyDemoAPI" {
+  name        = "MyDemoAPI"
+  description = "This is my API for demonstration purposes"
+}
+
+resource "aws_api_gateway_resource" "MyDemoResource" {
+  rest_api_id = aws_api_gateway_rest_api.MyDemoAPI.id
+  parent_id   = aws_api_gateway_rest_api.MyDemoAPI.root_resource_id
+  path_part   = "mydemoresource"
+}
+
+resource "aws_api_gateway_method" "MyDemoMethod" {
+  rest_api_id   = aws_api_gateway_rest_api.MyDemoAPI.id
+  resource_id   = aws_api_gateway_resource.MyDemoResource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "MyDemoIntegration" {
+  rest_api_id          = aws_api_gateway_rest_api.MyDemoAPI.id
+  resource_id          = aws_api_gateway_resource.MyDemoResource.id
+  http_method          = aws_api_gateway_method.MyDemoMethod.http_method
+  type                 = "MOCK"
+
+  request_parameters = {
+    "integration.request.header.X-Authorization" = "'static'"
+  }
+
+  # Transforms the incoming XML request to JSON
+  request_templates = {
+    "application/xml" = <<EOF
+{
+   "body" : $input.json('$')
+}
+EOF
+  }
+}
+
+variable "server_port" {
+  description = "Port that is used to run"
+  type        = number
+  default     = 8080
+}
+
+output "public_ip" {
+  value       = aws_instance.instance.public_ip
+  description = "Public server IP"
+}
